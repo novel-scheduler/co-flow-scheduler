a4786ace0f812b7bfe1a106dafc7e7712883b04e
bmkrutgers <44555410+bmkrutgers@users.noreply.github.com>
2022-09-03 03:37:54 +0530

    Add files via upload

--- fqtest.h
+++ fqtest.h
@@ -86,8 +86,52 @@ int retVal;
  return 1;
 
 }	
+
+
+
+
+void testpromotecoflows(struct Qdisc *sch, struct fq_sched_data *q )
+{
+
+    struct fq_flow f;
+    struct fq_flow g;
+    struct fq_flow h;
+    struct fq_flow i;
+    struct fq_flow j;
+    struct fq_flow coflow;
+    
+    f.sk = 1;
+    g.sk = 2;
+    h.sk = 3;
+    i.sk = 4;
+    j.sk = 5;
+    
+    f.socket_hash = f.sk +10;
+    
+    g.socket_hash = g.sk +10;
+    
+    i.socket_hash = i.sk +10;
+    
+    h.socket_hash = h.sk +10;
+    
+    j.socket_hash = j.sk +10;  
+    
+    fq_flow_add_tail(&q->new_flows, &f);
+    
+    fq_flow_add_tail(&q->new_flows, &g);
+    
+    fq_flow_add_tail(&q->new_flows, &h);
+    
+    //fq_flow_add_tail(&q->old_flows, &j);
+    
+    unsigned arr[] = {11,12};
+    
+    Promotecoflows(&q->old_flows, &q->new_flows,&q->co_flows, &f, &coflow, arr);
+
+
+}
 	
-void testfq(void)
+void testfq(struct Qdisc *sch, struct fq_sched_data *q )
 
 {
 
@@ -103,6 +147,9 @@ if(vResettest)
 printk("Reset Array test  Passed");
 else
 printk("Reset Arraytest Failed");
+
+
+testpromotecoflows(sch,q);
 
 }
 
